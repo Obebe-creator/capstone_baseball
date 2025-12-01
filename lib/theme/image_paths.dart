a4291e4960c94f75f3e60943c64ecb681e12a3fa
Obebe <shkim000905@naver.com>
2025-12-01 17:03:58 +0900

    로고 작업 시작

--- lib/theme/image_paths.dart
+++ lib/theme/image_paths.dart
@@ -1,6 +1,15 @@
 class ImagePaths {
   // MARK: 로고
   static String get mainLogo => 'assets/images/HO·UNG.png';
+  static String get ktLogo => 'assets/images/kt_logo.png';
+  static String get ssgLogo => 'assets/images/ssg_logo.png';
+  static String get kiaLogo => 'assets/images/kia_logo.png';
+  static String get doosanLogo => 'assets/images/doosan_logo.png';
+  static String get lotteLogo => 'assets/images/lotte_logo.png';
+  static String get samsungLogo => 'assets/images/samsung_logo.png';
+  static String get ncLogo => 'assets/images/nc_logo.png';
+  static String get kiwoomLogo => 'assets/images/kiwoom_logo.png';
+  static String get hanwhaLogo => 'assets/images/hanwha_logo.png';
 
   // MARK: Bot_nav_icon
   static String get navCalendar => 'assets/images/calendar-check.png';
--- lib/view/setting/setting_main.dart
+++ lib/view/setting/setting_main.dart
@@ -1,10 +1,34 @@
+import 'package:capstone_baseball/theme/app_colors.dart';
+import 'package:capstone_baseball/theme/font_styles.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SettingMain extends StatelessWidget {
   const SettingMain({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _body();
+  }
+
+  Widget _body() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 12.h),
+          // top widget
+          Text(
+            '마이 페이지',
+            style: FontStyles.KBO_bold_17.copyWith(color: AppColors.grey_title),
+          ),
+          SizedBox(height: 82.h),
+          // 선택 팀 로고
+          Container(),
+          // 응원 팀 선택
+          Text('응원 팀:', style: FontStyles.KBO_medium_13),
+        ],
+      ),
+    );
   }
 }
